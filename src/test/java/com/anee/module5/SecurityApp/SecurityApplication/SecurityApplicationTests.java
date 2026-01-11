c0454d87d1899c9108388bb744eb6e3914d94d84
Aneervan Ray <aneervan.raja2000@gmail.com>
2026-01-11 14:07:21 +0530

    Initial commit

---
+++ src/test/java/com/anee/module5/SecurityApp/SecurityApplication/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anee.module5.SecurityApp.SecurityApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
